361471bfe343f362f7f3b04ed6d1801e6987de70
IAmGroots <hadiseptian.crb@gmail.com>
2022-11-11 20:42:18 +0800

    coba lagi ah

--- a.py
+++ a.py
@@ -1,5 +1,8 @@
+print("Bukan Yang Ini")
 print("Ini Lagi Uji Coba")
 <<<<<<< HEAD
 print("Tapi Ini Dari Local")
+print("Bukan Yang Ini Juga")
 =======
 >>>>>>> refs/remotes/origin/main
+print("Tapi Yang Ini")
